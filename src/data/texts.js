f7851cb17371473eca1d9ed76184ac6c0b64a9dd
rageofpseudo <pierrejouffroy974@gmail.com>
2022-11-27 16:18:54 +0100

    Update texts.js

--- src/data/texts.js
+++ src/data/texts.js
@@ -2,12 +2,32 @@
 
 const introText = "<h2>(Cliquez pour jouer)</h2> <br> Bienvenue dans le Careli's Game. <br><br> Vous avez 10 ans pour récupérer des données et réguler la population d'animaux de la région au travers de choix qui s'afficheront sous forme de carte à chaque tour. <br><br> Vous aurez accès aux opinions de chaque acteurs de la région et devez agir en fonction. Tous les cinq tours vous aurez accès à des \"relevés scientifiques\" qui vous permettront de voir l'évolution de la population et des récoltes.";
 
-const hunterGameOver = "Les chasseurs sont en contradiction avec le consortium, ils ne suivent pas les indications du consortium et chassent à tout va. Les cadavres d'animaux se multiplient, des coups de balles près des maisons sont retrouvés. Les gens vivent dans la peur. C'est l'ère de la terreur.";
+const hunterGameOver = "Les chasseurs sont en contradiction avec le consortium, " +
+    "ils ne suivent pas les indications du consortium et chassent à tout va. " +
+    "Les cadavres d'animaux se multiplient, des coups de feux près des maisons sont retrouvés. " +
+    "Les gens vivent dans la peur. C'est l'ère de la terreur.";
 
-const farmerGameOver = "Les agriculteurs sont en contradiction avec le consortium,  ils ne suivent pas les indications du consortium et mettent des pièges partout. Des espèces disparaissent de plus en plus. On retrouve beaucoup de cadavres d'animaux près des fermes. De plus les gens se plaignent de plus en plus de l'abondance grandissant de piège. Les blessés se comptent par milliers. C'est l'ère des mutilés.";
+const farmerGameOver = "Les agriculteurs sont en contradiction avec le consortium, " +
+    " ils ne suivent pas les indications du consortium et mettent des pièges partout. " +
+    "Des espèces disparaissent de plus en plus. " +
+    "On retrouve beaucoup de cadavres d'animaux près des fermes. " +
+    "De plus les gens se plaignent de plus en plus de l'abondance grandissant de piège. Les blessés se comptent par milliers. " +
+    "C'est l'ère des mutilés.";
 
-const researcherGameOver = "Les chercheurs sont en contradiction avec le consortium,  ils ne suivent pas les indications du consortium et décident de faire ce qu'ils veulent. Ils sabotent les chasseurs et les agriculteurs, on retrouve des feux près des fermes, des pièges à portée humaine sont retrouvés dans les bois.  Les expériences sur les animaux se comptent par milliers. C'est l'ère de la recherche.";
 
-const naturalistGameOver = "Les naturalistes sont en contradiction avec le consortium,  ils ne suivent pas les indications du consortium et décident de protéger eux-mêmes la nature ! Ils commencent par saboter les chasseurs et les agriculteurs. Ils démolissent les infrastructures d'armes et les réserves de foin. De plus les animaux reprennent de plus en plus de territoire. C'est l'hécatombe. C'est le retour de l'Âge de Pierre.";
+// maladie prolifère et les animaux meurent
+// explosion
+// polluer l'eau
+const researcherGameOver = "Les chercheurs sont en contradiction avec le consortium,  " +
+    "ils ne suivent pas les indications du consortium et décident de faire ce qu'ils veulent." +
+    " Ils sabotent les chasseurs et les agriculteurs, on retrouve des feux près des fermes, " +
+    "des pièges à portée humaine sont retrouvés dans les bois.  " +
+    "Les expériences sur les animaux se comptent par milliers. C'est l'ère de la recherche.";
+
+const naturalistGameOver = "Les naturalistes sont en contradiction avec le consortium, " +
+    "ils ne suivent pas les indications du consortium et décident de protéger eux-mêmes la nature ! " +
+    "Ils commencent par saboter les chasseurs et les agriculteurs. " +
+    "Ils démolissent les infrastructures d'armes et les réserves de foin. De plus les animaux reprennent de plus en plus de territoire. " +
+    "C'est l'hécatombe. C'est le retour de l'Âge de Pierre.";
 
 const balancedGameOver = "L'équilibre entre tous les acteurs du consortium est maintenu, les chasseurs ont trouvé leur point d'équilibre de chasse, les agriculteurs cultivent en paix, les chercheurs continuent de faire des expériences plus sombres les unes que les autres, les naturalistes acceptent le bilan avec chagrin. Vous avez réussi à garder une harmonie dans l'environnement naturel et entre tous les acteurs ! Bravo !"
